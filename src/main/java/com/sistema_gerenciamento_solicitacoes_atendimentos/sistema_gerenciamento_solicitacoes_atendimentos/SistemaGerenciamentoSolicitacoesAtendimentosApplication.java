8923bad5982df15ee8883655e58c64e5797db74a
Renzolucas <mineorigiteste@Gmail.com>
2026-06-26 21:34:51 -0300

    meu primeiro commit:sincronizando o repositorio

---
+++ src/main/java/com/sistema_gerenciamento_solicitacoes_atendimentos/sistema_gerenciamento_solicitacoes_atendimentos/SistemaGerenciamentoSolicitacoesAtendimentosApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento_solicitacoes_atendimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoSolicitacoesAtendimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoSolicitacoesAtendimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_gerenciamento_solicitacoes_atendimentos/sistema_gerenciamento_solicitacoes_atendimentos/SistemaGerenciamentoSolicitacoesAtendimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento_solicitacoes_atendimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoSolicitacoesAtendimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
