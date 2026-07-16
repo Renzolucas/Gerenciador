504bedd5c5666d67bcb29827d079789c6a61c2c7
Renzolucas <mineorigiteste@Gmail.com>
2026-07-16 14:48:38 -0300

    ADICIONANDO ROTAS BUSCAR POR ID E DELETE

--- src/main/java/com/sistema_gerenciamento_solicitacoes_atendimentos/sistema_gerenciamento_solicitacoes_atendimentos/services/TaskService.java
+++ src/main/java/com/sistema_gerenciamento_solicitacoes_atendimentos/sistema_gerenciamento_solicitacoes_atendimentos/services/TaskService.java
@@ -1,5 +1,9 @@
 package com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento_solicitacoes_atendimentos.services;
 
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento_solicitacoes_atendimentos.domain.task.Task;
@@ -10,14 +14,15 @@ import com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento
 import com.sistema_gerenciamento_solicitacoes_atendimentos.sistema_gerenciamento_solicitacoes_atendimentos.domain.task.dtos.ResponseTaskBodyDTO;
 @Service
 public class TaskService {
-    private final TaskRepository taskRepositoty;
+    private final TaskRepository taskRepository;
     //PEGAMOS O USERSREPOSITORY PQ É LA QUE FICA O ID DO USUARIO
     private final UsersRepository usersRepository;
-    public TaskService(TaskRepository taskRepositoty, UsersRepository usersRepository) {
-        this.taskRepositoty = taskRepositoty;
+    public TaskService(TaskRepository taskRepository, UsersRepository usersRepository) {
+        this.taskRepository = taskRepository;
         this.usersRepository = usersRepository;
     }
-    
+
+    //PARTE LOGICA DE CRIAR UMA TASK
     public ResponseTaskBodyDTO createTask(CreateTaskBodyDTO dto){
         //CRIA UM ESPAÇO NA ENTIDADE PARA AS NOVAS INFORMAÇÕES NO BANCO
         Task novaTask = new Task();
@@ -37,11 +42,10 @@ public class TaskService {
         novaTask.setDeadline(dto.deadline());
         novaTask.setStatus(dto.status());
         novaTask.setPriority(dto.priority());
-
-        
-
         //ATUALIZANDO O BANCO
-        novaTask = taskRepositoty.save(novaTask);
+        novaTask = taskRepository.save(novaTask);
+
+        //CRIAMOS UMA NOVA "CARTA"/DTO PARA OCUTAR AS INFORMAÇÕES DOS IDs INFORMADOS.
         return new ResponseTaskBodyDTO(
         novaTask.getTitulo(),
         novaTask.getDescricao(),
@@ -50,5 +54,41 @@ public class TaskService {
         novaTask.getPriority()
         );
     }
-    
+    //PARTE LOGICA DE LISTAR TUDO
+    public List<ResponseTaskBodyDTO> buscarTask(){
+        //BUSCAMOS TUDO
+        List<Task> taskBrutas = taskRepository.findAll();
+        //JOGAMOS NA ESTEIRA NO STREAM, O MAP RECEBE TRANSFORMA OS DADOS
+        return taskBrutas.stream().map(task -> new ResponseTaskBodyDTO(
+            task.getTitulo(),
+            task.getDescricao(),
+            task.getDeadline(),
+            task.getStatus(),
+            task.getPriority()
+        )).collect(Collectors.toList());// COLECT COLETA E TRANSFORMA SEU TIPO
+    }
+
+    //PARTE LOGICA DO BUSCAR POR ID
+    public ResponseTaskBodyDTO buscarTaskPorId(UUID id){
+        //BUSCA O ID DA TASK
+        Task buscarTask = taskRepository.findById(id)
+            .orElseThrow(()-> new RuntimeException("ID DA TASK NAO ENCONTRADO"));
+        //SE SIM, CHAMAMOS A ESTRUTURA DO JSON DE RETORNO
+        return new ResponseTaskBodyDTO(
+            buscarTask.getTitulo(),
+            buscarTask.getDescricao(),
+            buscarTask.getDeadline(),
+            buscarTask.getStatus(),
+            buscarTask.getPriority()
+        );
+    }
+    //PARTE LOGICA DO DELETE
+    public void deletarTask(UUID id){
+        //BUSCA O ID DA TASK
+        Task buscarTask = taskRepository.findById(id)
+            .orElseThrow(()-> new RuntimeException("ID NAO ENCONTRADO"));
+        //SE SIM, DELETAMOS O USUARIO
+        taskRepository.delete(buscarTask);
+    }
+
 }
